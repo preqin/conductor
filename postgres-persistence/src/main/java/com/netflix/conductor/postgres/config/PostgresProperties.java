7342f624071055f1337ce101d2a0314929c7655b
Ben Pirt <ben@pirt.co.uk>
2024-03-05 15:10:24 +0000

    Implementation of Postgres PollDataDAO with read and write caching (#82)

--- postgres-persistence/src/main/java/com/netflix/conductor/postgres/config/PostgresProperties.java
+++ postgres-persistence/src/main/java/com/netflix/conductor/postgres/config/PostgresProperties.java
@@ -27,6 +27,12 @@ public class PostgresProperties {
 
     private Integer deadlockRetryMax = 3;
 
+    @DurationUnit(ChronoUnit.MILLIS)
+    private Duration pollDataFlushInterval = Duration.ofMillis(0);
+
+    @DurationUnit(ChronoUnit.MILLIS)
+    private Duration pollDataCacheValidityPeriod = Duration.ofMillis(0);
+
     public String schema = "public";
 
     public Duration getTaskDefCacheRefreshInterval() {
@@ -51,5 +57,53 @@ public class PostgresProperties {
 
     public void setSchema(String schema) {
         this.schema = schema;
+    }
+
+    public boolean getAllowFullTextQueries() {
+        return allowFullTextQueries;
+    }
+
+    public void setAllowFullTextQueries(boolean allowFullTextQueries) {
+        this.allowFullTextQueries = allowFullTextQueries;
+    }
+
+    public boolean getAllowJsonQueries() {
+        return allowJsonQueries;
+    }
+
+    public void setAllowJsonQueries(boolean allowJsonQueries) {
+        this.allowJsonQueries = allowJsonQueries;
+    }
+
+    public int getAsyncWorkerQueueSize() {
+        return asyncWorkerQueueSize;
+    }
+
+    public void setAsyncWorkerQueueSize(int asyncWorkerQueueSize) {
+        this.asyncWorkerQueueSize = asyncWorkerQueueSize;
+    }
+
+    public int getAsyncMaxPoolSize() {
+        return asyncMaxPoolSize;
+    }
+
+    public void setAsyncMaxPoolSize(int asyncMaxPoolSize) {
+        this.asyncMaxPoolSize = asyncMaxPoolSize;
+    }
+
+    public Duration getPollDataFlushInterval() {
+        return pollDataFlushInterval;
+    }
+
+    public void setPollDataFlushInterval(Duration interval) {
+        this.pollDataFlushInterval = interval;
+    }
+
+    public Duration getPollDataCacheValidityPeriod() {
+        return pollDataCacheValidityPeriod;
+    }
+
+    public void setPollDataCacheValidityPeriod(Duration period) {
+        this.pollDataCacheValidityPeriod = period;
     }
 }
